0c60ceb74d1575c182b4cebfd46f46fed2bbcced
jeonkwangsoo <jeonkwangsoo@naver.com>
2023-09-24 12:55:32 +0900

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+function buttonhome() {
+  document.location.href = "index.html";
+}
+function buttonpolar1() {
+  document.location.href = "polar1.html";
+}
+function buttonpolar2() {
+  document.location.href = "polar2.html";
+}
